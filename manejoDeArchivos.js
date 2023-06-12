9011c0c73bbf16d0b66f12f1833cac181eb81029
JS2C <juansecas1997@gmail.com>
2023-06-11 22:24:11 -0500

    NEW FEATURE: Se añade UPDATE_PRODUCTS y DELETE_PRODUTS

--- manejoDeArchivos.js
+++ manejoDeArchivos.js
@@ -37,35 +37,83 @@ class ProductManager {
     //Se obtiene los productos masivamente
     async getProducts() {
         const objs = await this.readFile(this.path, this.nameFile);
-        if(objs.length != 0){
+        if (objs.length != 0) {
             // console.log('getProducts: ', objs);
             return objs;
-        }    
+        }
         console.log('No hay información en eñ archivo');
     }//END GET_PRODUCTS
 
     //Se obtiene el producto por el id del mismo
     async getProductId(id) {
         const objs = await this.readFile(this.path, this.nameFile);
-        if (objs.length != 0){
+        if (objs.length != 0) {
             const obj = objs.find(data => data.id == id);
             if (!Boolean(obj)) console.log('No existe el producto con el id: ' + id);
             // console.log(obj);
             return obj;
         }
         console.log('No hay información en eñ archivo');
-     
+
     }//END GET_PRODUCT_ID
 
-    async updateProduct(id, updateData) {
+    //Actualiza un producto de los existentes
+    async updateProduct(id, __title, __description, __price, __thumbnail, __code, __stock) {
         console.log(Boolean(''));
-        this.getProducts().then( objs => {
-            const obj = objs.find(data => data.id == id);        
-            if(Boolean(obj)){
-               console.log(updateData);
+        this.getProducts().then(objs => {
+
+            const obj = objs.find(data => data.id == id);
+
+            if (Boolean(obj)) {
+                const positionObj = objs.indexOf(obj);
+                console.log('position: ', positionObj);
+                // console.log(obj);
+                const { id, title, description, price, thumbnail, code, stock } = obj;
+
+                let title_change = '';
+                let description_change = '';
+                let price_chage = 0;
+                let thumbnail_chage = '';
+                let code_change = '';
+                let stock_change = '';
+
+                Boolean(__title) ? title_change = __title : title_change = title;
+                Boolean(__description) ? description_change = __description : description_change = description;
+                Boolean(__price) ? price_chage = __price : price_chage = Number(price);
+                Boolean(__thumbnail) ? thumbnail_chage = __thumbnail : thumbnail_chage = thumbnail;
+                Boolean(__code) ? code_change = __code : code_change = code;
+                Boolean(__stock) ? stock_change = __stock : stock_change = Number(stock);
+
+                const obj_update = { id, title_change, description_change, price_chage, thumbnail_chage, code_change, stock_change };
+                objs.splice(positionObj, 1, obj_update);
+                // console.log('**********************');
+                // console.log('OBJs: ', objs);
+                // console.log('*********************');
+                // console.log(obj_update)
+                this.writeFile(this.path, this.nameFile, objs);
+                console.log('Se actualizó información, validar archivo');
+
+            } else {
+                console.log('No existe el producto para actualizar');
             }
         });
-    }
+    }//END UPDATE-PRODUCT
+
+    async deleteProduct(id) {
+        const objs = await this.getProducts();
+        const obj = objs.find(data => data.id == id);
+
+        if (Boolean(obj)) {
+            const positionObj = objs.indexOf(obj);
+            objs.splice(positionObj, 1);
+            console.log('Se eliminó información, validar archivo');
+            await this.writeFile(this.path, this.nameFile, objs);
+        } else {
+            console.log('No existe el producto para eliminar');
+        }
+
+    }//END DELETE-PRODUCT
+
 
     /**********************************************************************************************************
      **********************************************************************************************************
@@ -152,9 +200,9 @@ class ProductManager {
 const product1 = new ProductManager();
 // product1.getProducts();
 // product1.getProductId(1);
-const updateData = {title: "Hola", description: "aaaa"}
-product1.updateProduct(1, updateData);
-// product1.addProduct('madera', 'Sustancia dura y fibrosa', 50000, 'madera.png', 'RB77', 45);
+// product1.updateProduct(4, '', '', 666, 'rrr', '69A', '4');
+// product1.deleteProduct(4);
+product1.addProduct('madera', 'Sustancia dura y fibrosa', 50000, 'madera.png', 'RB77', 45);
 
 
 const product2 = new ProductManager();
